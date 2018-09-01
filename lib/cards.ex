22216345f463c16dcee2c638680108a23368ca7b
luizambrozio <oliveira.ambrozio@gmail.com>
2018-09-01 14:34:57 -0300

    add create_deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -12,7 +12,20 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamond"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
